7d2dfe0ca9173a3296c7ff6ad55c2c1153f1ac77
Jmenzies722 <Jmenzies722@gmail.com>
2023-11-06 14:46:30 -0500

    connected API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {},
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  container: {
+    center: true,
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  extend: {},
 };
+// eslint-disable-next-line no-undef
+export const plugins = [require("tailwind-scrollbar-hide")];
